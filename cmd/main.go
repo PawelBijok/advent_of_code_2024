b5ee341a9dae0bb38157067a9b02ac3a73660ecb
Paweł Bijok <bijok.pawel@gmail.com>
2024-12-02 20:20:59 +0100

    add timer

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/pawelbijok/advent2024/days/day_1"
 	"github.com/pawelbijok/advent2024/days/day_2"
 )
@@ -8,6 +11,7 @@ import (
 func main() {
 	day := 2
 	part := 2
+	start := time.Now().UnixNano()
 	if day == 1 {
 		if part == 1 {
 			day_1.Part1("./days/day_1/input.txt")
@@ -21,5 +25,17 @@ func main() {
 		} else {
 			day_2.Part2("./days/day_2/input.txt")
 		}
+	}
+	end := time.Now().UnixNano()
+	elapsed := end - start
+	describeTime(elapsed)
+}
+
+func describeTime(elapsed int64) {
+	if elapsed < 1000000 {
+		fmt.Printf("Time: %d ns\n", elapsed)
+	} else {
+
+		fmt.Printf("Time: %.3f ms\n", float64(elapsed)/1000000)
 	}
 }
